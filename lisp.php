95b07c720388eda837b3b7913f43d03e3305f226
Anssi Kulju <anssi.kulju@tuuri.fi>
2017-09-11 15:34:05 +0300

    Lot of fixing, redesign, refactoring, cleaning up
    -made things a lot simpler
    
    -got rid of stupid stuff and replaced them with more efficient routines

--- lisp.php
+++ lisp.php
@@ -20,21 +20,29 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 */
 
-define('DEBUG', true);
+define('DEBUG', false);
 
 class InternalFunctionDef
 {
 	public $symbol;
 	public $fn;
 	public $argc;
+	public $opts;
+
+	public function callFn($ctx, $args)
+	{
+		$list = '('.$this->symbol.' '.implode(' ', $args).')';
+		return call_user_func($this->fn, 
+			array('ctx' => $ctx, 'symbol' => $this->symbol, 'list' => $list, 'args' => $args));
+	}
 }
 
 class UserFunctionDef
 {
 	public $symbol;
-	public $def_list;
 	public $param_list;
 	public $body_list;
+	public $opts;
 }
 
 class StackNode
@@ -69,22 +77,22 @@ class LispContext
                 return null;
         }
 
-	public function addInternalFn($symbol, $fn_cb, $argc)
+	public function addInternalFn($symbol, $fn_cb, $argc, $opts = array())
 	{
 		$fn_d = new InternalFunctionDef();
 
 		$fn_d->symbol = $symbol;
 		$fn_d->fn = $fn_cb;
+		$fn_d->opts = $opts;
 
 		$this->internal_fns[$symbol] = $fn_d;
 	}
 
-	public function addUserFn($symbol, $def_list, $param_list, $body_list)
+	public function addUserFn($symbol, $param_list, $body_list)
 	{
 		$fn_d = new UserFunctionDef();
 
 		$fn_d->symbol = $symbol;
-		$fn_d->def_list = $def_list;
 		$fn_d->param_list = $param_list;
 		$fn_d->body_list = $body_list;
 
@@ -107,151 +115,87 @@ class LispContext
 		return null;
 	}
 
-	public function callInternalFn($symbol, $list)
+	public function callUserFn($components)
 	{
-		if (!isset($this->internal_fns[$symbol]))
-			return false;
+		$fn_symbol = reset($components);
+		$args = array();
 
-		return call_user_func($this->internal_fns[$symbol]->fn, 
-			array('ctx' => &$this, 'symbol' => $symbol, 'list' => $list));
-	}
-
-	public function callUserFn($symbol, $list)
-	{
-		if (!isset($this->user_fns[$symbol]))
-			return false;
-
-		$fn_d = $this->user_fns[$symbol];
-
-		$matches = array();
-
-		$pattern = '/\('.$symbol.'[\s]/';
-		$r = preg_match($pattern, $list, $matches);
-		if (!$r)
+		if (!isset($this->user_fns[$fn_symbol]))
 		{
-			dbg_log('Error: function call syntax incorrect in "%s"', $list);
-
-			return false;
-		}
-
-		$arg_list = '('.substr($list, strlen($matches[0]));
-
-		$params = list_explode($fn_d->param_list);
-		$args = list_explode($arg_list);
-
-		$args = array_combine($params, $args);
-                if ($args === false)
-                {
-                        err_log('Error, argument count incorrect in %s', $list);
+                        err_log('Error, user function not found with symbol %s', $fn_symbol);
 
                         return false;
-                }
+		}
+
+		$fn_d = $this->user_fns[$fn_symbol];
+
+
+		$arg_values = array_slice($components, 1);
+
+		$parameters = get_components($fn_d->param_list);
+		foreach ($parameters as $key => $p)
+		{
+			if (isset($arg_values[$key]))
+				$args[$p] = $arg_values[$key];
+		}
 
 		$body_list = $fn_d->body_list;
-
-                $n = new StackNode();
-
-                $n->args = $args;
-                $n->vars = array();
-                
-		foreach ($n->args as $key => $val)
+		foreach ($args as $key => $val)
 		{
 			$body_list = preg_replace('/'.preg_quote(' '.$key.' ').'/', ' '.$val.' ', $body_list);
 			$body_list = preg_replace('/'.preg_quote('('.$key.' ').'/', '('.$val.' ', $body_list);
 			$body_list = preg_replace('/'.preg_quote(' '.$key.')').'/', ' '.$val.')', $body_list);
 		}
 
+                $n = new StackNode();
+
+                $n->args = $args;
+                $n->vars = array();
+
                 $this->stackPush($n);
 
-                $stms = explode_statements(substr($body_list, 1, -1));
-                foreach ($stms as $stm)
-                {
-                        $r = process_statement($this, $stm);
-                        if ($r === false)
-                        {
-                                err_log('Error, process_statement failed with list: %s', $body_list);
-                        }
-                }
+		$r = process_statement($this, $body_list);
+		if ($r === false)
+		{
+			err_log('Error, process_statement failed with list: %s', $body_list);
+		}
 
                 $this->stackPop();
 
-                return $r;
+		return $r;
 	}
 }
-
-function list_explode($list)
-{
-	return explode(" ", substr($list, 1, -1));
-}
-
-function clean($list)
-{
-	$matches = array();
-
-	$list = preg_replace('/[\s]+/', ' ', $list);
-	$list = preg_replace('/^\s/', '', $list);
-	$list = preg_replace('/\s$/', '', $list);
-
-	$list = preg_replace('/\(\s/', '(', $list);
-	$list = preg_replace('/\s\)/', ')', $list);
-
-	dbg_log('Statement cleaned: "%s"', $list);
-
-	return $list;
-}
-
+	 
 function __fn_plus($args)
 { 
 	$ctx = $args['ctx'];
 	$symbol = $args['symbol'];
-	$list = $args['list'];
-	$arg = '('.substr($list, strlen($symbol) + 2);
+	$args = $args['args'];
 
-        $eval_stm = $arg;
+	if (count($args) != 2)
+	{
+		err_log('Error, incorrect number of args in +: %s', print_r($args, true));
 
-        $r = preg_match('/\(([^\)]+)\)/', $eval_stm, $matches);
-        if (!$r || count($matches) < 2)
-        {
-                err_log('Error in __fn_plus list evaluation: %s', $eval_stm);
-        }
+		return false;
+	}
 
-        $sum = array_sum(explode(' ', $matches[1]));
-
-        return $sum;
+        return array_sum($args);
 }
 
 function __fn_print($args)
 { 
 	$ctx = $args['ctx'];
 	$symbol = $args['symbol'];
-	$list = $args['list'];
-	$arg = substr($list, strlen($symbol) + 2, -1);
+	$args = $args['args'];
 
-        $eval_stm = $arg;
+	if (count($args) != 1)
+	{
+		err_log('Error, incorrect number of args in print: %s', print_r($args, true));
 
-        /*
-	while (1)
-	{ 
-		$l = get_inner_list($eval_stm);
-
-		if ($l && $l != $eval_stm)
-		{
-			$r = process_statement($ctx, $l);
-
-			$eval_stm = preg_replace('/'.preg_quote($l).'/', $r, $eval_stm);
-                        $eval_stm = clean($eval_stm);
-
-                        continue;
-                }
-
-                $r = process_statement($ctx, $l);
-
-
-                break;
+		return false;
 	}
-         */
 
-        echo $arg."\n";
+        echo $args[0]."\n";
 
 	return null;
 }
@@ -260,125 +204,147 @@ function __fn_defun($args)
 { 
 	$ctx = $args['ctx'];
 	$symbol = $args['symbol'];
-	$list = $args['list'];
+	$args = $args['args'];
 
-	$matches = array();
-
-	$pattern = '/'.preg_quote($symbol).'\s([^\s]+)\s(\([^\)]*\))\s/';
-
-	$r = preg_match($pattern, $list, $matches);
-	if (count($matches) < 3)
-	{ 
-		dbg_log('Error: defun syntax incorrect in "%s"', $list);
+	if (count($args) != 3)
+	{
+		err_log('Error, incorrect number of args in defun: %s', print_r($args, true));
 
 		return false;
 	}
 
-	$userfn_sym = $matches[1];
-	$arg_list = $matches[2];
+	$userfn_sym = $args[0];
+	$arg_list = $args[1];
+	$fn_body = $args[2];
 
-	$fn_body = substr($list, strlen($matches[0]) + 1, -1);
-
-	$ctx->addUserFn($userfn_sym, $list, $arg_list, $fn_body);
+	$ctx->addUserFn($userfn_sym, $arg_list, $fn_body);
 }
 
 function __fn_let($args)
 {
 	$ctx = $args['ctx'];
 	$symbol = $args['symbol'];
-	$list = $args['list'];
-	$arg_list = substr($list, strlen($symbol) + 2, -1);
+	$args = $args['args'];
 
-        $arg_arr = explode_statements($arg_list);
-        if (count($arg_arr) != 2)
+	if (count($args) != 2)
+	{
+		err_log('Error, incorrect number of arguments: %s', print_r($args, true));
+
+		return false;
+	}
+
+	$binds = get_components($args[0]);
+	foreach ($binds as $key => $b)
+	{
+		$variable = get_components($b);
+
+		if (count($variable) != 2)
+		{
+			err_log('Error, incorrect variable bind: %s', print_r($variable, true));
+
+			return false;
+		}
+
+		$c = get_components($variable[1]);
+		if (count($c) > 1)
+		{
+			$r = process_statement($ctx, $variable[1]);
+
+			$binds[$key] = create_list(array($variable[0], $r));
+		}
+	}
+
+        $body_list = $args[1];
+
+        foreach ($binds as $bind)
         {
-                err_log('Error in __fn_let: incorrect syntax in %s', $list);
+		$b = get_components($bind);
 
-                return false;
-        }
+		$key = $b[0];
+		$val = $b[1];
 
-        $let_arg_lists = explode_statements(substr($arg_arr[0], 1, -1));
-
-        $let_args = array();
-        foreach ($let_arg_lists as $l)
-        {
-                $tmp = explode(' ', substr($l, 1, -1));
-                if (count($tmp) != 2)
-                {
-                        err_log('Error in __fn_let: incorrect syntax in variable definition (%s)', $l);
-
-                        return false;
-                }
-
-                $let_args[$tmp[0]] = $tmp[1];
-        }
-
-        $body_list = $arg_arr[1];
-
-        foreach ($let_args as $key => $val)
-        {
                 $body_list = preg_replace('/'.preg_quote(' '.$key.' ').'/', ' '.$val.' ', $body_list);
                 $body_list = preg_replace('/'.preg_quote('('.$key.' ').'/', '('.$val.' ', $body_list);
                 $body_list = preg_replace('/'.preg_quote(' '.$key.')').'/', ' '.$val.')', $body_list);
         }
 
-	$stms = explode_statements(substr($body_list, 1, -1));
-	foreach ($stms as $stm)
-        {
-		$r = process_statement($ctx, $stm);
-		if ($r === false)
-		{
-			err_log('Error, process_statement failed with list: %s', $body_list);
-		}
-        }
+	$r = process_statement($ctx, $body_list);
+	if ($r === false)
+	{
+		err_log('Error, process_statement failed with list: %s', $body_list);
+	}
 
         return $r;
 }
 
-function get_inner_list($stm)
+function call_function($ctx, $components = array())
 {
-	if (!$stm)
-		return false;
-
-	$matches = array();
-	$pattern = '/\(([^\(\)])+\)/';
-
-	$r = preg_match($pattern, $stm, $matches);
-
-	if ($r && count($matches) > 1)
+	if (count($components) == 0)
 	{
-		return $matches[0];
+		return false;
 	}
 
-	return $stm;
-}
+	$fn_symbol = $components[0];
 
-function get_fn_sym_from_list($list)
-{
-	$matches = array();
+	$type = 'internal';
 
-        $pattern = '/^\(([^\(\)\s]+)\s/';
-
-	$r = preg_match($pattern, $list, $matches);
-	if (!$r)
-        {
-                dbg_log('Function symbol not found in %s', $list);
-
-                return false;
-        }
-
-        return $matches[1];
-}
-
-function get_args_from_list($list, $argc)
-{
-	$frags = explode_statements(substr($list, 1, -1));
-	if (count($frags) != $argc)
+	$fn = $ctx->getInternalFn($fn_symbol);
+	if (!$fn)
 	{
-		err_log('Error, incorrect number of args in %s', $list);
+		$fn = $ctx->getUserFn($fn_symbol);
+		$type = 'user';
+	}
+
+	if (!$fn)
+	{
+		err_log('Error, function symbol %s is not defined', $fn_symbol);
 
 		return false;
 	}
+
+	dbg_log('call_function "%s" with components: %s', $fn_symbol, print_r($components, true));
+
+	$args = array_slice($components, 1);
+
+	if (!isset($fn->opts['no_arg_eval']) || !$fn->opts['no_arg_eval'])
+	{
+		foreach ($args as $key => $a)
+		{
+			$comps = get_components($a);
+
+			$sym = reset($comps);
+			if ($sym)
+			{
+				$a_fn = $ctx->getInternalFn($sym);
+				if (!$a_fn)
+				{
+					$a_fn = $ctx->getUserFn($sym);
+				}
+
+				if ($a_fn)
+				{
+					$a_args = array_slice($comps, 1);
+
+					$r = call_function($ctx, $comps);
+
+					dbg_log('call_function result: %s', $r);
+
+					$args[$key] = $r;
+
+					dbg_log('arguments after replacing: %s ', print_r($args, true));
+				}
+			}
+		}
+	}
+
+	dbg_log('actually calling "%s" with args %s', $fn_symbol, print_r($args, true));
+
+	if ($type == 'internal')
+		$r = $fn->callFn($ctx, $args);
+	else
+		$r = $ctx->callUserFn($components);
+
+	return $r;
 }
 
 function process_statement(&$context, $stm)
@@ -388,51 +354,10 @@ function process_statement(&$context, $stm)
 	if (!$stm)
 		return false;
 
-        $eval_stm = $stm;
-	while (1)
-	{ 
-		$l = get_inner_list($eval_stm);
-
-		if ($l && $l != $eval_stm)
-		{
-			$r = process_statement($context, $l);
-
-			$eval_stm = preg_replace('/'.preg_quote($l).'/', $r, $eval_stm);
-                        $eval_stm = clean($eval_stm);
-
-			dbg_log('Statement evald: %s -> %s', $stm, $eval_stm);
-
-                        continue;
-                }
-
-                break;
-	}
-
-        $stm = $eval_stm;
-
-        $s_node = $context->stackGetTop();
-        if (!$s_node)
-        {
-                err_log('Stack not initiated');
-                
-                return false;
-        }
-
-	$fn_symbol = get_fn_sym_from_list($stm);
-
-	$fn = $context->getInternalFn($fn_symbol);
-	if ($fn)
+	$comps = get_components($stm);
+	if (count($comps) >= 2)
 	{
-		$r = $context->callInternalFn($fn_symbol, $stm);
-
-		return $r;
-	}
-
-	$fn = $context->getUserFn($fn_symbol);
-	if ($fn)
-	{
-		$r = $context->callUserFn($fn_symbol, $stm);
-
+		$r = call_function($context, $comps);
 		return $r;
 	}
 
@@ -476,7 +401,6 @@ function dbg_log($fmt)
 	$line = date('Y-m-d H:i:s').' '.$prefix.$string."\n";
 
 	echo $line;
-
 }
 
 function read_src_file($filename)
@@ -502,88 +426,94 @@ function read_src_file($filename)
         return $r;
 }
 
-function explode_statements($src)
+function create_list($components)
 {
-        $ready_for_end = true;
-        $starting_par = false;
-        $par_c = 0;
-        $current = '';
-        $statements = array();
+	return '('.implode(' ', $components).')';
+}
 
-        for ($i = 0; $i < strlen($src); $i++)
-        {
-                if (trim($src[$i]) == '')
-                        $is_wspc = true;
-                else
-                        $is_wspc = false;
+function clean($list)
+{
+	$matches = array();
 
-                switch ($src[$i])
-                {
-                case '(':
-                        if (!$starting_par)
-                        {
-                                $starting_par = true;
-                        }
+	$list = preg_replace('/[\s]+/', ' ', $list);
+	$list = preg_replace('/^\s/', '', $list);
+	$list = preg_replace('/\s$/', '', $list);
 
-                        $ready_for_end = false;
-                        $par_c++;
+	$list = preg_replace('/\(\s/', '(', $list);
+	$list = preg_replace('/\s\)/', ')', $list);
 
-                        break;
+	dbg_log('Statement cleaned: "%s"', $list);
 
-                case ')':
-                        $par_c--;
+	return $list;
+}
 
-                        if ($par_c == 0)
-                                $ready_for_end = true;
 
-                        break;
-                }
+function get_components($stm)
+{
+	$components = array();
+	$current = '';
+	$p_count = 0;
 
-                if (!$starting_par && $src[$i] != '(' && !$is_wspc)
-                {
-                        err_log('Syntax error, no starting par found (%s)', $src[$i]);
+	if ($stm[0] != '(' || $stm[strlen($stm) - 1] != ')')
+	{
+		dbg_log('Statement "%s" is not a list', $stm);
 
-                        return false;
-                }
+		return array($stm);
+	}
 
-                if ($par_c < 0)
-                {
-                        err_log('Syntax error, par_c = -1');
+	$stm = substr($stm, 1, -1);
+	for ($i = 0; $i < strlen($stm); $i++)
+	{
+                if (trim($stm[$i]) == '')
+		{
+			if ($current && $p_count == 0)
+			{
+				$components[] = $current;
+				$current = '';
 
-                        return false;
-                }
+				continue;
+			}
+		}
 
-                $current .= $src[$i];
+		if ($stm[$i] == '(')
+		{
+			$p_count++;
+		}
 
-                if ($ready_for_end && $current)
-                {
-                        if (!preg_match('/^[\s]*$/', $current))
-                                $statements[] = $current;
+		if ($stm[$i] == ')')
+		{
+			$p_count--;
 
-                        $current = '';
-                }
-        }
+			if ($p_count == 0 && $current)
+			{
+				$current .= $stm[$i];
+				$components[] = $current;;
+				$current = '';
 
-        if (!$ready_for_end)
-        {
-                err_log('Errors found in source with par_c %d', $par_c);
+				continue;
+			}
+		}
 
-                return false;
-        }
+		if (!$current && trim($stm[$i]) == '')
+			continue;
+		
+		$current .= $stm[$i];
+	}
 
-        dbg_log('No error found in source');
+	if ($current)
+		$components[] = $current;
 
-        return $statements;
+	return $components;
 }
 
 function execute()
 {
 	$ctx = new LispContext();
 
-	$ctx->addInternalFn('defun', '__fn_defun', 4);
+	$ctx->addInternalFn('defun', '__fn_defun', 4, array('no_arg_eval' => true));
 	$ctx->addInternalFn('+', '__fn_plus', 3);
 	$ctx->addInternalFn('print', '__fn_print', 2);
-	$ctx->addInternalFn('let', '__fn_let', 3);
+	$ctx->addInternalFn('let', '__fn_let', 3, array('no_arg_eval' => true));
 
         $filename = 'testscript.lisp';
 
@@ -597,33 +527,12 @@ function execute()
                 return false;
         }
 
-        $stms = explode_statements($src);
+	$stms = get_components('('.$src.')');
         foreach ($stms as $s)
         {
                 if (!preg_match('/^[\s]*$/', $s))
                 {
                         $stm = clean($s);
-
-			$fn_symbol = get_fn_sym_from_list($stm);
-			$fn = $ctx->getInternalFn($fn_symbol);
-
-			$r = get_args_from_list($stm, count($fn->args));
-			var_dump($r);
-			die();
-
-			if ($fn_symbol == 'defun')
-			{
-				$ctx->callInternalFn($fn_symbol, $stm);
-
-				continue;
-			}
-
-			if ($fn_symbol == 'let')
-			{
-				$ctx->callInternalFn($fn_symbol, $stm);
-
-				continue;
-			}
 
                         $r = process_statement($ctx, $s);
 
--- testscript.lisp
+++ testscript.lisp
@@ -1,6 +1,6 @@
-(defun test (a b) ((print 2) (+ a b 1)))
+(defun test (a b) (+ (+ a b) 1))
 
-(print (+ (+ 2 (+ 5 6)) (+ 7 (+ (test 3 6) 7)))) 
+(print (+ 7 (+ (test 3 6) 7)))
 (print hei)
 
 (let ((a 2) (b 7)) (+ a (+ b 1)))
